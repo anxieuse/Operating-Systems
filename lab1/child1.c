b750070faab0b76af5456e158ecb44b073f09685
anxieuse <64018836+anxieuse@users.noreply.github.com>
2020-10-10 02:25:28 +0300

    Add files via upload

--- lab1/child1.c
+++ lab1/child1.c
@@ -1,42 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <strings.h>
 #include <string.h>
 #include <ctype.h>
 
 #define INBUFSIZE 300 // Buffer size
 
-int check_vowel(char t)
+int isVowel(char t)
 {
     t = tolower(t);
     if (t == 'a' || t == 'e' || t == 'i' || t == 'o' || t == 'u' || t == 'y')
         return 1;
-
     return 0;
 }
 
 int main(void)
 {
     char buff[INBUFSIZE], res[INBUFSIZE];
-    pid_t mypid;
 
-    mypid = getpid(); // Process-ID
+    pid_t mypid = getpid(); 
     printf("1st Child %d: Started!\n", mypid);
 
     while (fgets(buff, INBUFSIZE, stdin))
     {
-        printf("I got new line: %s", buff);
+        printf("Received line: %s", buff);
+
         int d = 0;
         for (int i = 0; i < strlen(buff); ++i)
         {
-            if (!check_vowel(buff[i]))
+            if (!isVowel(buff[i]))
             {
                 res[d++] = buff[i];
             }
         }
         res[d] = '\0';
+        
         printf("Processed line: %s", res);
     }
     printf("1st Child %d: I'm Done!\n", mypid);
--- lab1/main.c
+++ lab1/main.c
@@ -1,13 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <time.h>
-#include <errno.h>
 #include <fcntl.h>
 #include <string.h>
-
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 
 #define MAX_FNAME_LENGTH 100
@@ -18,6 +13,12 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc < 3)
+    {
+        printf("Usage: ./main filename1 filename2\n");
+        exit(-1);
+    }
+
     char filename1[MAX_FNAME_LENGTH], filename2[MAX_FNAME_LENGTH];
     printf("Name of the 1st child-output file: %s\n", argv[1]);
     printf("Name of the 2nd child-output file: %s\n", argv[2]);
@@ -30,7 +31,7 @@ int main(int argc, char *argv[])
         if (pipe(fd[i]) < 0)
         {
             perror("Pipe error");
-            exit(1); 
+            exit(1);
         }
     }
 
@@ -43,7 +44,7 @@ int main(int argc, char *argv[])
         exit(1);
     }
     else if (pid1 == 0)
-    {   // child1
+    { // child1
         // closing useless pipes
         close(fd[1][0]);
         close(fd[1][1]);
@@ -86,37 +87,43 @@ int main(int argc, char *argv[])
         exit(1);
     }
     else if (pid2 == 0)
-    {   // child2
+    { // child2
         // closing useless pipes
         close(fd[0][0]);
         close(fd[0][1]);
         close(fd[1][WR]);
 
         int file_out = open(filename2, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-        if (file_out < 0) {
+        if (file_out < 0)
+        {
             perror("File err");
             exit(1);
         }
 
         int new_out = dup2(file_out, STDOUT_FILENO);
         close(file_out);
-        if (new_out < 0) {
+        if (new_out < 0)
+        {
             perror("Duping child2 stdout err");
             exit(1);
         }
 
         int new_in = dup2(fd[1][RD], STDIN_FILENO);
         close(fd[1][RD]);
-        if (new_in < 0) {
+        if (new_in < 0)
+        {
             perror("Duping child1 stdin err");
             exit(1);
         }
 
-        if (execlp("./child2", "child2", NULL) < 0) {
+        if (execlp("./child2", "child2", NULL) < 0)
+        {
             perror("Execl err");
             exit(1);
         }
     }
+
+    char *eol;
 
     close(fd[0][RD]);
     close(fd[1][RD]);
@@ -125,10 +132,12 @@ int main(int argc, char *argv[])
     char msg[MAX_STR_LENGTH];
     while (fgets(msg, MAX_STR_LENGTH, stdin))
     {
-        if(strlen(msg) > 10) {
+        if (strlen(msg) > 10)
+        {
             write(fd[1][WR], msg, strlen(msg));
         }
-        else {
+        else
+        {
             write(fd[0][WR], msg, strlen(msg));
         }
     }
