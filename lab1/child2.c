df12f6b6b0496e8bb1e6c1f1bd75e26c0beb3314
anxieuse <64018836+anxieuse@users.noreply.github.com>
2020-10-16 18:29:17 +0300

    Add files via upload

---
+++ lab1/child2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+
+#define INBUFSIZE 300 // Buffer size
+
+int isVowel(char t)
+{
+    t = tolower(t);
+    if (t == 'a' || t == 'e' || t == 'i' || t == 'o' || t == 'u' || t == 'y')
+        return 1;
+    return 0;
+}
+
+int main(void)
+{
+    char buff[INBUFSIZE], res[INBUFSIZE];
+
+    pid_t mypid = getpid();
+    printf("2nd Child %d: Started!\n", mypid);
+
+    while (fgets(buff, INBUFSIZE, stdin))
+    {
+        printf("Received line: %s", buff);
+
+        int d = 0;
+        for (int i = 0; i < strlen(buff); ++i)
+        {
+            if (!isVowel(buff[i]))
+            {
+                res[d++] = buff[i];
+            }
+        }
+        res[d] = '\0';
+
+        printf("Processed line: %s", res);
+    }
+    printf("2nd Child %d: I'm Done!\n", mypid);
+
+    return 0;
+}
